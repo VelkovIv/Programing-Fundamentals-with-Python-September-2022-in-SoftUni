d5dfcc6fe6016585aad77d54fe139e17cea28bf5
Ivaylo Velkov <114020789+VelkovIv@users.noreply.github.com>
2022-12-05 19:53:21 +0200

    Next solutions

---
+++ 02.data_types_and_variables_-exe/01.integer_operations.py
@@ -0,0 +1,6 @@
+first_number = int(input())
+second_number = int(input())
+third_number = int(input())
+fourd_number = int(input())
+final_number = int((first_number + second_number) / third_number) * fourd_number
+print(final_number)
---
+++ 02.data_types_and_variables_-exe/02.chars_to_string.py
@@ -0,0 +1,4 @@
+charter_one = input()
+charter_two = input()
+charter_three = input()
+print(f'{charter_one}{charter_two}{charter_three}')
