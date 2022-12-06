8fce722efeb4994bc9da61f3e345be5d6407501d
Ivaylo Velkov <114020789+VelkovIv@users.noreply.github.com>
2022-12-06 18:27:20 +0200

    next solution

---
+++ 02.data_types_and_variables_-exe/05.print_part_of_the_ASCII_table.py
@@ -0,0 +1,5 @@
+first_index = int(input())
+last_index = int(input())
+for currect_index in range(first_index, last_index + 1):
+    currect_chair = chr(currect_index)
+    print(currect_chair, end=' ')
